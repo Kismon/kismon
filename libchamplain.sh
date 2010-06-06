24653e849143a2b116d4e1877fa090b36e04308a
Patrick Salecker <mail@salecker.org>
2010-06-06 13:44:25 +0200

    fix dependencies in the install instructions

--- libchamplain.sh
+++ libchamplain.sh
@@ -4,7 +4,7 @@ cd champlain
 # install general dependenies
 sudo apt-get install git-core autoconf libtool gtk-doc-tools libsqlite3-dev libclutter-1.0-dev libsoup-gnome2.4-dev libclutter-gtk-0.10-dev
 # install python dependenies
-sudo apt-get install python-dev python-gobject-dev python-gtk2 python-clutter-dev python-clutter-gtk-dev python-cairo-dev
+sudo apt-get install python-dev python-gobject-dev python-gtk2-dev python-clutter-dev python-clutter-gtk-dev python-cairo-dev
 
 # download and install libmemphis
 wget https://trac.openstreetmap.ch/trac/memphis/downloads/3 --no-check-certificate -O memphis-0.2.1.tar.gz 
