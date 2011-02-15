5e85891ad5ded4d15fdc7c66b4073b858c2fc947
Patrick Salecker <mail@salecker.org>
2011-02-15 22:46:30 +0100

    move some buttons to the map

--- kismon/map.py
+++ kismon/map.py
@@ -55,7 +55,8 @@ class Map:
 			"red": clutter.Color(255, 0, 0, 220),
 			"green": clutter.Color(0, 255, 0, 220),
 			"orange":clutter.Color(243, 148, 7, 220),
-			"black": clutter.Color(0, 0, 0, 255)
+			"black": clutter.Color(0, 0, 0, 255),
+			"white": clutter.Color(255, 255, 255, 220),
 			}
 		if os.path.isdir("/usr/share/kismon"):
 			self.share_folder = "/usr/share/kismon/"
@@ -68,6 +69,7 @@ class Map:
 		self.textures = {}
 		self.load_images()
 		self.create_dots()
+		self.create_right_group()
 		
 		self.marker_layer = {}
 		for color in ("red", "orange", "green"):
@@ -79,7 +81,7 @@ class Map:
 		self.view.add_layer(self.position_layer)
 		
 		margin = 5
-		padding = 20
+		padding = 10
 		
 		label = clutter.Group()
 		label_bg = clutter.Rectangle()
@@ -103,6 +105,82 @@ class Map:
 		self.map_source_factory = champlain.map_source_factory_dup_default()
 		self.map_data_source = None
 		self.apply_config()
+		
+		self.view.connect("allocation-changed", self.on_resize)
+		self.view.connect("button-press-event", self.on_map_pressed)
+		self.view.connect("button-release-event", self.on_map_released)
+		
+	def on_resize(self, actor, box, flags):
+		gobject.idle_add(self.move_right_group)
+		
+	def create_right_group(self):
+		margin = 5
+		padding = 4
+		start = 0
+		view_width, view_height = self.view.get_size()
+		right_group = clutter.Group()
+		
+		for text in ("in", "out"):
+			size = 28
+			texture = clutter.CairoTexture(width=size, height=size)
+			context = texture.cairo_create()
+			context.set_source_color(self.colors["white"])
+			context.arc(size/2, size/2, size/2-1, 0, 3.14*2)
+			context.fill()
+			context.stroke()
+			
+			context.set_source_color(self.colors["black"])
+			context.arc(size/2, size/2, size/2-1, 0, 3.14*2)
+			context.set_line_width(1)
+			context.stroke()
+			
+			context.set_line_width(3)
+			if text == "in":
+				context.move_to(size/2, 5)
+				context.line_to(size/2, size - 5)
+				
+			context.move_to(5, size/2)
+			context.line_to(size - 5, size/2)
+			
+			context.stroke()
+			del(context)
+			
+			texture.set_position(20 + start, 28)
+			texture.set_reactive(True)
+			if text == "in":
+				texture.connect('button-release-event', self.zoom_in)
+			else:
+				texture.connect('button-release-event', self.zoom_out)
+			right_group.add(texture)
+			start += 32
+		
+		label = clutter.Group()
+		label_bg = clutter.Rectangle()
+		label_bg.set_color(clutter.color_from_string('white'))
+		label_bg.set_opacity(180)
+		label.add(label_bg)
+
+		label_text = clutter.Text('Sans 10', "Follow GPS: on", \
+			clutter.color_from_string('black'))
+		label.add(label_text)
+		self.follow_label = label_text
+
+		width, height = label_text.get_size()
+		label_bg.set_size(width + padding * 3, height + padding * 2)
+		label_bg.set_position(0, 0)
+		label_text.set_position(padding , padding)
+		label.show()
+		label.set_reactive(True)
+		label.connect('button-release-event', self.on_toggle_moving)
+		right_group.add(label)
+		
+		self.view.add(right_group)
+		self.right_group = right_group
+		self.move_right_group()
+		
+	def move_right_group(self):
+		view_width, view_height = self.view.get_size()
+		self.right_group.set_position(view_width - 110, 8)
 		
 	def apply_config(self):
 		if self.config["source"] == "memphis-local":
@@ -137,10 +215,10 @@ class Map:
 	def set_zoom(self, zoom):
 		self.view.set_property("zoom-level", zoom)
 		
-	def zoom_in(self):
+	def zoom_in(self, actor=None, event=None, view=None):
 		self.view.zoom_in()
 		
-	def zoom_out(self):
+	def zoom_out(self, actor=None, event=None, view=None):
 		self.view.zoom_out()
 		
 	def set_position(self, lat, lon):
@@ -283,6 +361,29 @@ class Map:
 		self.set_position(lat, lon)
 		self.next_position = None
 		
+	def on_toggle_moving(self, actor=None, event=None):
+		if self.follow_label is None:
+			return
+		text = "Follow GPS: %s"
+		if self.config["follow_gps"] is True:
+			self.stop_moving()
+			self.follow_label.set_text(text % "off")
+		else:
+			self.start_moving()
+			self.follow_label.set_text(text % "on")
+		
+	def on_map_pressed(self, actor, event):
+		"""disable set_position if the map is pressed
+		"""
+		if self.config["follow_gps"] is True:
+			self.stop_moving()
+		
+	def on_map_released(self, actor, event):
+		
+		text = self.follow_label.get_text()
+		if self.config["follow_gps"] is False and "on" in text:
+			self.start_moving()
+		
 	def locate_marker(self, key):
 		if key not in self.markers:
 			print "marker %s not found" % key
@@ -398,44 +499,18 @@ class MapWidget:
 		self.config = config
 		
 		self.embed = champlaingtk.ChamplainEmbed()
-		self.embed.connect("button-press-event", self.on_map_pressed)
-		self.embed.connect("button-release-event", self.on_map_released)
-		
 		self.view = self.embed.get_view()
 		self.map = Map(self.config, self.view, memphis)
 		
 		self.vbox = gtk.VBox()
 		self.init_menu()
 		self.vbox.add(self.embed)
-		self.map.toggle_moving_button = self.toggle_moving_button
 		
 		self.widget = self.vbox
 		
 	def init_menu(self):
 		hbox = gtk.HBox()
 		self.vbox.pack_start(hbox, expand=False, fill=False, padding=0)
-		
-		button = gtk.Button()
-		image = gtk.Image()
-		image.set_from_stock(gtk.STOCK_ZOOM_IN, gtk.ICON_SIZE_MENU)
-		button.set_image(image)
-		button.connect("clicked", self.on_zoom_in)
-		button.show()
-		hbox.pack_start(button, expand=False, fill=False, padding=0)
-		
-		button = gtk.Button()
-		image = gtk.Image()
-		image.set_from_stock(gtk.STOCK_ZOOM_OUT, gtk.ICON_SIZE_MENU)
-		button.set_image(image)
-		button.connect("clicked", self.on_zoom_out)
-		button.show()
-		hbox.pack_start(button, expand=False, fill=False, padding=10)
-		
-		self.toggle_moving_button = gtk.CheckButton("Follow GPS")
-		self.toggle_moving_button.connect("clicked", self.on_toggle_moving)
-		self.toggle_moving_button.show()
-		self.toggle_moving_button.set_active(True)
-		hbox.add(self.toggle_moving_button)
 		
 		label = gtk.Label("Marker style:")
 		hbox.add(label)
@@ -450,34 +525,10 @@ class MapWidget:
 			combobox.set_active(1)
 		hbox.add(combobox)
 		
-	def on_map_pressed(self, widget, event):
-		"""disable set_position if the map is pressed
-		"""
-		if self.config["follow_gps"] is True:
-			self.map.stop_moving()
-		
-	def on_map_released(self, widget, event):
-		active = self.toggle_moving_button.get_active()
-		if self.config["follow_gps"] is False and active is True:
-			self.map.start_moving()
-		
 	def on_change_marker_style(self, widget):
 		style = widget.get_active_text().lower()[:-1]
 		self.map.set_marker_style(style)
-		
-	def on_toggle_moving(self, widget):
-		active = widget.get_active()
-		if active is True:
-			self.map.start_moving()
-		else:
-			self.map.stop_moving()
-		
-	def on_zoom_in(self, widget):
-		self.map.zoom_in()
-		
-	def on_zoom_out(self, widget):
-		self.map.zoom_out()
-	
+
 if __name__ == "__main__":
 	import test
 	test.map()
