536609bcaf5e70469e39c943e7b1785803f2d538
Patrick Salecker <mail@salecker.org>
2020-04-05 14:46:28 +0200

    Avoid crash when device has no dot11.device field

--- kismon/core.py
+++ kismon/core.py
@@ -292,7 +292,7 @@ class Core:
         queue = thread.get_queue("dot11")
         for x in range(0, len(queue)):
             device = queue.pop(0)
-            if device['dot11.device'] == 0: # skip non-802.11 devices
+            if 'dot11.device' not in device or device['dot11.device'] == 0: # skip non-802.11 devices
                 continue
             self.networks.add_device_data(device, server_id)
             mac = device['kismet.device.base.macaddr']
